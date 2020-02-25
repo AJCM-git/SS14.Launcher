abe16b5831307c3f28696641e50622f85bdcc7fd
Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
2020-02-25 22:44:54 +0100

    Crappy Steam Experiments

--- SS14.Launcher/Program.cs
+++ SS14.Launcher/Program.cs
@@ -1,20 +1,46 @@
-﻿using Avalonia;
+﻿using System;
+using System.Runtime.InteropServices;
+using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Logging.Serilog;
 using Avalonia.ReactiveUI;
 using SS14.Launcher.Models;
 using SS14.Launcher.ViewModels;
 using SS14.Launcher.Views;
+using Steamworks;
 
 namespace SS14.Launcher
 {
     internal static class Program
     {
+        private static readonly bool EnableSteam = true;
+        private const int SteamAppId = 1255460;
+
         // Initialization code. Don't use any Avalonia, third-party APIs or any
         // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
         // yet and stuff might break.
         public static void Main(string[] args)
         {
+            NativeLibrary.SetDllImportResolver(typeof(SteamClient).Assembly, (name, assembly, path) =>
+            {
+                if (name != "steam_api64")
+                {
+                    return IntPtr.Zero;
+                }
+
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                {
+                    return NativeLibrary.Load("./libsteam_api.so");
+                }
+
+                return IntPtr.Zero;
+            });
+
+            if (EnableSteam)
+            {
+                SteamClient.Init(SteamAppId);
+            }
+
             BuildAvaloniaApp().Start(AppMain, args);
         }
 
@@ -39,6 +65,7 @@ namespace SS14.Launcher
             {
                 DataContext = viewModel
             };
+
             viewModel.OnWindowInitialized();
 
             app.Run(window);
